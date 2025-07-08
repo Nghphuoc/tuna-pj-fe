eaf5f1cbde097c7d68400a35e1d009d3867667b2
PhuocHN <huuphuocit04@gmail.com>
2025-07-08 16:49:42 +0700

    remove: space

--- app/components/MN-S01/errornotifycation.jsx
+++ app/components/MN-S01/errornotifycation.jsx
@@ -5,7 +5,8 @@ const ErrorNotification = ({ message }) => {
     const notifycation = message || "処理に失敗しました。お手数ですが再度お試しください。解消しない場合はサポートまでお問い合わせください。";
 
     return (
-        <div className=" w-full">
+        
+        <div className="w-fill">
             <div className="rounded-md shadow-lg w-full mb-2 mt-2 bg-red-200 border border-red-500">
                 <p className="text-sm text-red-500 m-2 ml-6">
                     <BsFillExclamationCircleFill className="inline-block mr-2" />
