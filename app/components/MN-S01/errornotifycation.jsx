66384ae99554c3e92932bf855c9ab2fd3695511d
PhuocHN <huuphuocit04@gmail.com>
2025-07-08 15:34:48 +0700

    update: icon for errornotifycation

--- app/components/MN-S01/errornotifycation.jsx
+++ app/components/MN-S01/errornotifycation.jsx
@@ -1,11 +1,16 @@
 "use client";
+import { BsFillExclamationCircleFill } from "react-icons/bs";
 
 const ErrorNotification = ({ message, onClose }) => {
     const notifycation = message || "処理に失敗しました。お手数ですが再度お試しください。解消しない場合はサポートまでお問い合わせください。";
+
     return (
         <div className=" w-full">
             <div className="rounded-md shadow-lg w-full mb-2 mt-2 bg-red-200 border border-red-500">
-                <p className="text-sm text-red-500 m-2 ml-6">{notifycation}</p>
+                <p className="text-sm text-red-500 m-2 ml-6">
+                    <BsFillExclamationCircleFill className="inline-block mr-2" />
+                    {notifycation}
+                </p>
             </div>
         </div>
     );
