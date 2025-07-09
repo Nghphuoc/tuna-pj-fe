32571010e93816fa27ded0efb194c21083aba4b2
PhuocHN <huuphuocit04@gmail.com>
2025-07-09 13:10:32 +0700

    remove: navigate global

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavigationFullItems from "./Components/navigationfullitems";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <NavigationFullItems />
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
