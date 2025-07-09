e9a6e21eeebd9e3da3c31cd902381774aacc3300
PhuocHN <huuphuocit04@gmail.com>
2025-07-09 09:54:01 +0700

    modify: css size for ErrorNotifycation

--- app/components/MN-S01/errornotifycation.jsx
+++ app/components/MN-S01/errornotifycation.jsx
@@ -5,8 +5,7 @@ const ErrorNotification = ({ message }) => {
     const notifycation = message || "処理に失敗しました。お手数ですが再度お試しください。解消しない場合はサポートまでお問い合わせください。";
 
     return (
-        
-        <div className="w-fill">
+        <div className="w-full h-full flex-col justify-center items-center">
             <div className="rounded-md shadow-lg w-full mb-2 mt-2 bg-red-200 border border-red-500">
                 <p className="text-sm text-red-500 m-2 ml-6">
                     <BsFillExclamationCircleFill className="inline-block mr-2" />
