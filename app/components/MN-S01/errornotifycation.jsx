378fb135df277f7bf967879874df38a109c4d120
PhuocHN <huuphuocit04@gmail.com>
2025-07-09 14:11:58 +0700

    modify: location icon

--- app/components/MN-S01/errornotifycation.jsx
+++ app/components/MN-S01/errornotifycation.jsx
@@ -7,8 +7,8 @@ const ErrorNotification = ({ message }) => {
     return (
         <div className="w-full h-full flex-col justify-center items-center">
             <div className="rounded-md shadow-lg w-full mb-2 mt-2 bg-red-200 border border-red-500">
-                <p className="text-sm text-red-500 m-2 ml-6">
-                    <BsFillExclamationCircleFill className="inline-block mr-2" />
+                <p className="text-sm text-red-500 m-2 ml-6 flex items-center">
+                    <BsFillExclamationCircleFill className="inline-block mr-2 mb-1 w-[20px] h-[20px]" />
                     {notifycation}
                 </p>
             </div>
